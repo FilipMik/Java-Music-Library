4f7292effa6539f3c0da4c017d26ed34d8c67898
Peter Ziska <peter@ziska.sk>
2018-10-14 10:38:12 +0200

    Basic maven project

---
+++ src/main/java/cz/muni/fi/pa165/music_library/Main.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.music_library;
+
+public class Main {
+
+    public static void main(String [ ] args)
+    {
+        System.out.println("Let's party!!");
+    }
+
+}
---
+++ src/test/java/cz/muni/fi/pa165/music_library/FirstTest.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.music_library;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void testConvert() {
+        assertEquals(1, 1);
+    }
+}
