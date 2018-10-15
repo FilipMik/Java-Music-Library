c30c0c58dee915ac11757064b3b27430fcfd8107
KlaraMinsterova <klara.minsterova@gmail.com>
2018-10-15 21:07:01 +0200

    user

---
+++ src/main/java/cz/muni/fi/pa165/music_library/data/entities/User.java
@@ -0,0 +1,81 @@
+package cz.muni.fi.pa165.music_library.data.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author Klara
+ */
+
+public class User {
+
+    private Integer userId;
+    private String userName;
+    private String email;
+    private Date dateCreated;
+    private UserLevel userLevel;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public UserLevel getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(UserLevel userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (userId != null ? !userId.equals(user.userId) : user.userId != null) return false;
+        if (userName != null ? !userName.equals(user.userName) : user.userName != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        if (dateCreated != null ? !dateCreated.equals(user.dateCreated) : user.dateCreated != null) return false;
+        return userLevel == user.userLevel;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
+        result = 31 * result + (userLevel != null ? userLevel.hashCode() : 0);
+        return result;
+    }
+}
