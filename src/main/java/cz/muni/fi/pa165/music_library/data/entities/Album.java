86dfaf17e0a01f9c536b2a1c98126d858d39d1d9
Filip Mik <filip.mik@thefuntasty.com>
2018-10-16 14:05:20 +0200

    Remove consturctor and add missing equals and hash code

--- src/main/java/cz/muni/fi/pa165/music_library/data/entities/Album.java
+++ src/main/java/cz/muni/fi/pa165/music_library/data/entities/Album.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @author Filip Mik on 16. 10. 2018
+ */
+
 public class Album {
     private Integer albumId;
     private Date releaseDate;
@@ -13,34 +17,45 @@ public class Album {
     private Integer songsCount;
     private Image albumArt;
 
-    public Album(Integer albumId, Date releaseDate, String title, String commentary, List<Song> songList, Image albumArt) {
-        this.albumId = albumId;
-        this.releaseDate = releaseDate;
-        this.title = title;
-        this.commentary = commentary;
-        this.songList = songList;
-        this.songsCount = songList.size();
-        this.albumArt = albumArt;
-    }
-
     public Integer getAlbumId() {
         return albumId;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
     }
 
     public Date getReleaseDate() {
         return releaseDate;
     }
 
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getCommentary() {
         return commentary;
     }
 
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
     public List<Song> getSongList() {
         return songList;
+    }
+
+    public void setSongList(List<Song> songList) {
+        this.songList = songList;
+        this.songsCount = songList.size();
     }
 
     public Integer getSongsCount() {
@@ -51,7 +66,39 @@ public class Album {
         return albumArt;
     }
 
-    public void setCommentary(String commentary) {
-        this.commentary = commentary;
+    public void setAlbumArt(Image albumArt) {
+        this.albumArt = albumArt;
+    }
+
+    public List<Song> getSongs() {
+        return this.songList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Album album = (Album) o;
+
+        if (albumId != null ? !albumId.equals(album.albumId) : album.albumId != null) return false;
+        if (releaseDate != null ? !releaseDate.equals(album.releaseDate) : album.releaseDate != null) return false;
+        if (title != null ? !title.equals(album.title) : album.title != null) return false;
+        if (commentary != null ? !commentary.equals(album.commentary) : album.commentary != null) return false;
+        if (songList != null ? !songList.equals(album.songList) : album.songList != null) return false;
+        if (songsCount != null ? !songsCount.equals(album.songsCount) : album.songsCount != null) return false;
+        return albumArt != null ? albumArt.equals(album.albumArt) : album.albumArt == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = albumId != null ? albumId.hashCode() : 0;
+        result = 31 * result + (releaseDate != null ? releaseDate.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (commentary != null ? commentary.hashCode() : 0);
+        result = 31 * result + (songList != null ? songList.hashCode() : 0);
+        result = 31 * result + (songsCount != null ? songsCount.hashCode() : 0);
+        result = 31 * result + (albumArt != null ? albumArt.hashCode() : 0);
+        return result;
     }
 }
--- src/main/java/cz/muni/fi/pa165/music_library/data/entities/Song.java
+++ src/main/java/cz/muni/fi/pa165/music_library/data/entities/Song.java
@@ -1,5 +1,11 @@
 package cz.muni.fi.pa165.music_library.data.entities;
 
+import java.util.Objects;
+
+/**
+ * @author Filip Mik on 16. 10. 2018
+ */
+
 public class Song {
     private Integer songId;
     private String title;
@@ -8,46 +14,78 @@ public class Song {
     private String commentary;
     private Genre genre;
 
-    public Song(Integer songId, String title, Integer bitRate, Integer albumPosition, String commentary, Genre genre) {
-        this.songId = songId;
-        this.title = title;
-        this.bitRate = bitRate;
-        this.albumPosition = albumPosition;
-        this.commentary = commentary;
-        this.genre = genre;
-    }
-
     public Integer getSongId() {
         return songId;
+    }
+
+    public void setSongId(Integer songId) {
+        this.songId = songId;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public Integer getBitRate() {
         return bitRate;
+    }
+
+    public void setBitRate(Integer bitRate) {
+        this.bitRate = bitRate;
     }
 
     public Integer getAlbumPosition() {
         return albumPosition;
     }
 
-    public String getCommentary() {
-        return commentary;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
     public void setAlbumPosition(Integer albumPosition) {
         this.albumPosition = albumPosition;
+    }
+
+    public String getCommentary() {
+        return commentary;
     }
 
     public void setCommentary(String commentary) {
         this.commentary = commentary;
     }
 
+    public Genre getGenre() {
+        return genre;
+    }
 
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Song song = (Song) o;
+
+        if (songId != null ? !songId.equals(song.songId) : song.songId != null) return false;
+        if (title != null ? !title.equals(song.title) : song.title != null) return false;
+        if (bitRate != null ? !bitRate.equals(song.bitRate) : song.bitRate != null) return false;
+        if (albumPosition != null ? !albumPosition.equals(song.albumPosition) : song.albumPosition != null)
+            return false;
+        if (commentary != null ? !commentary.equals(song.commentary) : song.commentary != null) return false;
+        return genre == song.genre;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = songId != null ? songId.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (bitRate != null ? bitRate.hashCode() : 0);
+        result = 31 * result + (albumPosition != null ? albumPosition.hashCode() : 0);
+        result = 31 * result + (commentary != null ? commentary.hashCode() : 0);
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        return result;
+    }
 }
