28ee70af40a2e3df183e905d33685c7f458dda3d
Filip Mik <filip.mik@thefuntasty.com>
2018-10-21 12:50:14 +0200

    Create AlbumDao methods \

--- src/main/java/cz/muni/fi/pa165/music_library/dao/implementations/AlbumDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/music_library/dao/implementations/AlbumDaoImpl.java
@@ -1,10 +1,47 @@
 package cz.muni.fi.pa165.music_library.dao.implementations;
 
 import cz.muni.fi.pa165.music_library.dao.interfaces.AlbumDao;
+import cz.muni.fi.pa165.music_library.data.entities.Album;
+
+import java.util.List;
 
 /**
  * @author Filip Mik on 21. 10. 2018
  */
 
 public class AlbumDaoImpl implements AlbumDao {
+
+    private List<Album> albums;
+
+    @Override
+    public List<Album> getAllALbums() {
+        return albums;
+    }
+
+    @Override
+    public Album getAlbum(int albumId) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Album getAlbumByTitle(int title) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void addAlbum(Album album) {
+        // TODO
+    }
+
+    @Override
+    public void deleteAlbum(int albumId) {
+        // TODO
+    }
+
+    @Override
+    public void updateAlbum(int albumId) {
+        // TODO
+    }
 }
--- src/main/java/cz/muni/fi/pa165/music_library/dao/interfaces/AlbumDao.java
+++ src/main/java/cz/muni/fi/pa165/music_library/dao/interfaces/AlbumDao.java
@@ -1,8 +1,25 @@
 package cz.muni.fi.pa165.music_library.dao.interfaces;
 
+import cz.muni.fi.pa165.music_library.data.entities.Album;
+
+import java.util.List;
+
 /**
  * @author Filip Mik on 21. 10. 2018
  */
 
 public interface AlbumDao {
+
+    List<Album> getAllALbums();
+
+    Album getAlbum(int albumId);
+
+    Album getAlbumByTitle(int title);
+
+    void addAlbum(Album album);
+
+    void deleteAlbum(int albumId);
+
+    void updateAlbum(int albumId);
+
 }
