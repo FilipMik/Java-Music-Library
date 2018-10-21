3b010024ce1071a848b35695b61f1d4496663c50
Filip Mik <filip.mik@thefuntasty.com>
2018-10-21 13:36:31 +0200

    Create playlist dao

--- src/main/java/cz/muni/fi/pa165/music_library/dao/implementations/PlayListDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/music_library/dao/implementations/PlayListDaoImpl.java
@@ -1,10 +1,64 @@
 package cz.muni.fi.pa165.music_library.dao.implementations;
 
 import cz.muni.fi.pa165.music_library.dao.interfaces.PlayListDao;
+import cz.muni.fi.pa165.music_library.data.entities.Playlist;
+import cz.muni.fi.pa165.music_library.data.entities.Song;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Filip Mik on 21. 10. 2018
  */
 
 public class PlayListDaoImpl implements PlayListDao {
+
+    private List<Playlist> playlists = new ArrayList<>();
+
+    public PlayListDaoImpl() {
+        // TODO implement
+    }
+
+    @Override
+    public List<Playlist> getAllPlaylists() {
+        return playlists;
+    }
+
+    @Override
+    public Playlist getPlaylistById(int playlistId) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public Boolean addPlaylist(Playlist playlist) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public void deletePlaylist(int playlistId) {
+        // TODO implement
+    }
+
+    @Override
+    public void updatePlaylistTitle(int playlistId, String title) {
+        // TODO implement
+    }
+
+    @Override
+    public void addSong(int playlistId, Song song) {
+        // TODO implement
+    }
+
+    @Override
+    public Boolean removeSong(int playlistId, Song song) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public void clearSongs(int playlistId) {
+        // TODO implement
+    }
 }
--- src/main/java/cz/muni/fi/pa165/music_library/dao/interfaces/PlayListDao.java
+++ src/main/java/cz/muni/fi/pa165/music_library/dao/interfaces/PlayListDao.java
@@ -1,8 +1,29 @@
 package cz.muni.fi.pa165.music_library.dao.interfaces;
 
+import cz.muni.fi.pa165.music_library.data.entities.Playlist;
+import cz.muni.fi.pa165.music_library.data.entities.Song;
+
+import java.util.List;
+
 /**
  * @author Filip Mik on 21. 10. 2018
  */
 
 public interface PlayListDao {
+
+    List<Playlist> getAllPlaylists();
+
+    Playlist getPlaylistById(int playlistId);
+
+    Boolean addPlaylist(Playlist playlist);
+
+    void deletePlaylist(int playlistId);
+
+    void updatePlaylistTitle(int playlistId, String title);
+
+    void addSong(int playlistId, Song song);
+
+    Boolean removeSong(int playlistId, Song song);
+
+    void clearSongs(int playlistId);
 }
