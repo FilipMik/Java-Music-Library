e93836a3d3d1f115a0e3c3db918e715e55058117
Filip Mik <filip.mik@thefuntasty.com>
2018-10-21 13:46:53 +0200

    Add user DAO

--- src/main/java/cz/muni/fi/pa165/music_library/dao/implementations/UserDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/music_library/dao/implementations/UserDaoImpl.java
@@ -1,10 +1,54 @@
 package cz.muni.fi.pa165.music_library.dao.implementations;
 
 import cz.muni.fi.pa165.music_library.dao.interfaces.UserDao;
+import cz.muni.fi.pa165.music_library.data.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Filip Mik on 21. 10. 2018
  */
 
 public class UserDaoImpl implements UserDao {
+
+    private List<User> users = new ArrayList<>();
+
+    public UserDaoImpl() {
+        // TODO implement
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    @Override
+    public User getUserById(int userId) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public User getUserByName(int username) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public Boolean AddUser(User user) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public Boolean deleteUser(User user) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public void updateUserEmail(int userId, String email) {
+        // TODO implement
+    }
 }
--- src/main/java/cz/muni/fi/pa165/music_library/dao/interfaces/UserDao.java
+++ src/main/java/cz/muni/fi/pa165/music_library/dao/interfaces/UserDao.java
@@ -1,8 +1,24 @@
 package cz.muni.fi.pa165.music_library.dao.interfaces;
 
+import cz.muni.fi.pa165.music_library.data.entities.User;
+
+import java.util.List;
+
 /**
  * @author Filip Mik on 21. 10. 2018
  */
 
 public interface UserDao {
+
+    List<User> getAllUsers();
+
+    User getUserById(int userId);
+
+    User getUserByName(int username);
+
+    Boolean AddUser(User user);
+
+    Boolean deleteUser(User user);
+
+    void updateUserEmail(int userId, String email);
 }
