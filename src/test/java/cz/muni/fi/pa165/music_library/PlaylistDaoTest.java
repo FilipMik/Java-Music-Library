17333b3ddd858688d640f4067d1599592f770836
Filip Mik <filip.mik@thefuntasty.com>
2018-11-04 12:08:59 +0100

    Add relations tests

--- src/test/java/cz/muni/fi/pa165/music_library/PlaylistDaoTest.java
+++ src/test/java/cz/muni/fi/pa165/music_library/PlaylistDaoTest.java
@@ -1,11 +1,9 @@
 package cz.muni.fi.pa165.music_library;
 
-import cz.muni.fi.pa165.music_library.dao.interfaces.ArtistDao;
-import cz.muni.fi.pa165.music_library.dao.interfaces.PlayListDao;
-import cz.muni.fi.pa165.music_library.dao.interfaces.SongDao;
-import cz.muni.fi.pa165.music_library.dao.interfaces.UserDao;
+import cz.muni.fi.pa165.music_library.dao.interfaces.*;
 import cz.muni.fi.pa165.music_library.data.entities.Playlist;
 import cz.muni.fi.pa165.music_library.data.entities.Song;
+import cz.muni.fi.pa165.music_library.data.entities.User;
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,6 +20,7 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 import javax.persistence.PersistenceException;
 import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Filip Mik on 3. 11. 2018
@@ -41,9 +40,6 @@ public class PlaylistDaoTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private UserDao userDao;
-
-    @Autowired
-    private ArtistDao artistDao;
 
     private Playlist chillPlaylist;
     private Playlist workPlaylist;
@@ -134,8 +130,78 @@ public class PlaylistDaoTest extends AbstractTestNGSpringContextTests {
     }
 
     @Test
-    public void relationTest() {
+    public void updateTest() {
+        playListDao.createPlaylist(workPlaylist);
+
+        Assert.assertEquals("Work playlist",
+                playListDao.getPlaylistById(workPlaylist.getPlaylistId()).getTitle());
+
+        workPlaylist.setTitle("New work playlist");
+        playListDao.updatePlaylist(workPlaylist);
+
+        Assert.assertNotEquals("Work playlist",
+                playListDao.getPlaylistById(workPlaylist.getPlaylistId()).getTitle());
+        Assert.assertEquals("New work playlist",
+                playListDao.getPlaylistById(workPlaylist.getPlaylistId()).getTitle());
+    }
+
+    @Test
+    public void testSongRelations() {
+        playListDao.createPlaylist(workPlaylist);
+
         Song song1 = new Song();
         Song song2 = new Song();
+
+        song1.setTitle("Song1");
+        song1.setCommentary("Song1 commentary");
+        song1.addPlaylist(playListDao.getPlaylistById(workPlaylist.getPlaylistId()));
+
+        song2.setTitle("Song2");
+        song2.setCommentary("Song2 commentary");
+        song2.addPlaylist(playListDao.getPlaylistById(workPlaylist.getPlaylistId()));
+
+        songDao.createSong(song1);
+        songDao.createSong(song2);
+
+        workPlaylist.addSong(songDao.getSongById(song1.getSongId()));
+        workPlaylist.addSong(songDao.getSongById(song2.getSongId()));
+
+        List<Song> foundSongList = playListDao.getPlaylistById(workPlaylist.getPlaylistId()).getSongList();
+
+        Assert.assertEquals(2L, foundSongList.size());
+        Assert.assertEquals("Song1", foundSongList.get(0).getTitle());
+        Assert.assertEquals("Song2 commentary", foundSongList.get(foundSongList.size() - 1).getCommentary());
+
+        Assert.assertEquals("Work playlist",
+                songDao.getSongById(song1.getSongId()).getPlaylists().get(0).getTitle());
+
+        workPlaylist.removeSong(songDao.getSongById(song1.getSongId()));
+        workPlaylist.removeSong(songDao.getSongById(song2.getSongId()));
+
+        Assert.assertEquals(0L, playListDao.getPlaylistById(workPlaylist.getPlaylistId()).getSongList().size());
+    }
+
+
+    @Test
+    public void testUserRelations() {
+        playListDao.createPlaylist(workPlaylist);
+
+        User user1 = new User();
+
+        user1.setEmail("sample@email.cz");
+        user1.setUsername("user1");
+        user1.addPlaylist(playListDao.getPlaylistById(workPlaylist.getPlaylistId()));
+
+        userDao.createUser(user1);
+
+        Assert.assertEquals(1L, userDao.getUserById(user1.getUserId()).getPlaylists().size());
+        workPlaylist.setUser(userDao.getUserById(user1.getUserId()));
+
+        Assert.assertEquals("user1",
+                playListDao.getPlaylistById(workPlaylist.getPlaylistId()).getUser().getUsername());
+
+        userDao.getUserById(user1.getUserId()).removePlaylist(playListDao.getPlaylistById(workPlaylist.getPlaylistId()));
+
+        Assert.assertEquals(0L, userDao.getUserById(user1.getUserId()).getPlaylists().size());
     }
 }
