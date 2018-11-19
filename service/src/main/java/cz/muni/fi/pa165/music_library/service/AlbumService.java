3e6bce910a35961b0bb2c3f5b430e982b8ba8bef
Peter Ziska <peter@ziska.sk>
2018-11-19 20:01:11 +0100

    Basic album service interface and implementation

---
+++ service/src/main/java/cz/muni/fi/pa165/music_library/service/AlbumService.java
@@ -0,0 +1,57 @@
+package cz.muni.fi.pa165.music_library.service;
+
+import cz.muni.fi.pa165.music_library.data.entities.Album;
+
+import java.util.List;
+
+/**
+ * @author Peter Žiška
+ * ID: 487569
+ */
+
+public interface AlbumService {
+
+    /**
+     * Returns list of albums with specified ID
+     * @param albumId Id of Album
+     * @return Album with album ID (unique)
+     */
+    Album getAlbumById(Long albumId);
+
+    /**
+     * Returns list of albums with specified title
+     *
+     * @param title Title of albums to be found
+     * @return List of Albums with specified title
+     */
+    List<Album> getAlbumByTitle(String title);
+
+    /**
+     * Return every album stored in DB
+     *
+     * @return List of Albums in DB
+     */
+    List<Album> getAllAlbums();
+
+    /**
+     * Deletes specified album
+     *
+     * @param album Album to be deleted
+     */
+    void deleteAlbum(Album album);
+
+    /**
+     * Updates specified album
+     *
+     * @param album Album to be updated
+     */
+    void updateAlbum(Album album);
+
+    /**
+     * Creates album
+     *
+     * @param album Album to be created
+     */
+    void createAlbum(Album album);
+
+}
---
+++ service/src/main/java/cz/muni/fi/pa165/music_library/service/AlbumServiceImpl.java
@@ -0,0 +1,45 @@
+package cz.muni.fi.pa165.music_library.service;
+
+import cz.muni.fi.pa165.music_library.dao.interfaces.AlbumDao;
+import cz.muni.fi.pa165.music_library.data.entities.Album;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlbumServiceImpl implements AlbumService{
+
+    @Autowired
+    private AlbumDao albumDao;
+
+    @Override
+    public Album getAlbumById(Long albumId) {
+        return albumDao.getAlbumById(albumId);
+    }
+
+    @Override
+    public List<Album> getAlbumByTitle(String title) {
+        return albumDao.getAlbumByTitle(title);
+    }
+
+    @Override
+    public List<Album> getAllAlbums() {
+        return albumDao.getAllAlbums();
+    }
+
+    @Override
+    public void deleteAlbum(Album album) {
+        albumDao.deleteAlbum(album);
+    }
+
+    @Override
+    public void updateAlbum(Album album) {
+        albumDao.updateAlbum(album);
+    }
+
+    @Override
+    public void createAlbum(Album album) {
+        albumDao.createAlbum(album);
+    }
+}
