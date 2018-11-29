db768b9390fe505efa8424f06aee88c9544d7a77
Peter Ziska <peter@ziska.sk>
2018-11-29 11:25:45 +0100

    Artist facade implementation

---
+++ service/src/main/java/cz/muni/fi/pa165/music_library/facade/ArtistFacadeImpl.java
@@ -0,0 +1,65 @@
+package cz.muni.fi.pa165.music_library.facade;
+
+import cz.muni.fi.pa165.music_library.data.entities.Artist;
+import cz.muni.fi.pa165.music_library.dto.ArtistDto;
+import cz.muni.fi.pa165.music_library.service.ArtistService;
+import cz.muni.fi.pa165.music_library.service.BeanMappingService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author Peter Žiška
+ * ID: 487569
+ */
+
+public class ArtistFacadeImpl implements ArtistFacade {
+
+    @Autowired
+    private ArtistService artistService;
+
+    @Autowired
+    private BeanMappingService mapper;
+
+    @Override
+    public List<ArtistDto> getAllArtists() {
+        List<Artist> artists = artistService.getAllArtists();
+        return (artists == null) ? null : mapper.mapTo(artists,ArtistDto.class);
+    }
+
+    @Override
+    public ArtistDto findArtistById(Long artistId) {
+        Artist artist = artistService.getArtistById(artistId);
+        return (artist == null) ? null : mapper.mapTo(artist,ArtistDto.class);
+    }
+
+    @Override
+    public List<ArtistDto> findArtistsByName(String name) {
+        List<Artist> artists = artistService.getArtistsByName(name);
+        return (artists == null) ? null : mapper.mapTo(artists,ArtistDto.class);
+    }
+
+    @Override
+    public void createArtist(ArtistDto artist) {
+        if (artist == null) {
+            throw new IllegalArgumentException("Album DTO shouldn't be null");
+        }
+        Artist artistCreate = mapper.mapTo(artist,Artist.class);
+        artistService.createArtist(artistCreate);
+    }
+
+    @Override
+    public void updateArtist(ArtistDto artist) {
+        if (artist == null) {
+            throw new IllegalArgumentException("Album DTO shouldn't be null");
+        }
+        Artist artistUpdate = mapper.mapTo(artist,Artist.class);
+        artistService.updateArtistInfo(artistUpdate);
+    }
+
+    @Override
+    public void deleteArtist(Long artistId) {
+        Artist artist = artistService.getArtistById(artistId);
+        artistService.deleteArtist(artist);
+    }
+}
