76e0500dff0da76a6baeace87d521686bab6b5a2
Jan Ficko <jan.ficko@gmail.com>
2018-12-14 16:50:27 +0100

    - Uncommented annotation

--- rest/src/main/java/cz/muni/fi/pa165/music_library/rest/exceptions/ResourceAlreadyExistingException.java
+++ rest/src/main/java/cz/muni/fi/pa165/music_library/rest/exceptions/ResourceAlreadyExistingException.java
@@ -1,6 +1,9 @@
 package cz.muni.fi.pa165.music_library.rest.exceptions;
 
-//@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason="The resource already exists")
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason="The resource already exists")
 public class ResourceAlreadyExistingException extends RuntimeException {
     
 } 
