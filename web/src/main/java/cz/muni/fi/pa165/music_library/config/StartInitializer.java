4a458833a29b0f93f2bcf19a21af6f570fa4a5ba
Filip Mik <filip.mik@thefuntasty.com>
2018-12-16 16:25:15 +0100

    Create initializer

---
+++ web/src/main/java/cz/muni/fi/pa165/music_library/config/StartInitializer.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.pa165.music_library.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Filip Mik on 16. 12. 2018
+ */
+
+public class StartInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        //TODO
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        //TODO
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        //TODO
+        return new String[0];
+    }
+}
