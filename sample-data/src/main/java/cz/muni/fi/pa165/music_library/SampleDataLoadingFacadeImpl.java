c2c33601262ea1f4a184771d66d740db7557c6d2
Filip Mik <filip.mik@thefuntasty.com>
2018-12-17 21:33:57 +0100

    Create sample data module

---
+++ sample-data/src/main/java/cz/muni/fi/pa165/music_library/SampleDataLoadingFacadeImpl.java
@@ -0,0 +1,91 @@
+package cz.muni.fi.pa165.music_library;
+
+import cz.muni.fi.pa165.music_library.data.entities.*;
+import cz.muni.fi.pa165.music_library.exceptions.EmailAlreadyExistsException;
+import cz.muni.fi.pa165.music_library.exceptions.UsernameAlreadyExistsException;
+import cz.muni.fi.pa165.music_library.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @author Filip Mik on 17. 12. 2018
+ */
+
+public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PlaylistService playlistService;
+
+    @Autowired
+    SongService songService;
+
+    @Autowired
+    AlbumService albumService;
+
+    @Autowired
+    ArtistService artistService;
+
+    User user;
+    Playlist playlist;
+    Album album;
+    Song song;
+    Artist artist;
+
+    @Override
+    public void loadData() throws IOException {
+        playlist();
+        artist();
+        album();
+        song();
+        try {
+            user();
+        } catch (EmailAlreadyExistsException e) {
+            e.printStackTrace();
+        } catch (UsernameAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private User user() throws EmailAlreadyExistsException, UsernameAlreadyExistsException {
+        user = new User();
+        user.setUsername("Filip");
+        user.setEmail("s@s.cz");
+        userService.registerUser(user, "aaaa");
+        return userService.findUserByEmail("s@s.cz");
+    }
+
+    private Artist artist() {
+        artist = new Artist();
+        artist.setName("heolda");
+        artistService.createArtist(artist);
+        return artistService.getArtistsByName("heolda").get(0);
+    }
+
+    private Song song() {
+        song = new Song();
+        song.setTitle("song1");
+        songService.createSong(song);
+        return songService.getSongsByTitle("song1").get(0);
+    }
+
+    private Playlist playlist() {
+        Playlist playlist = new Playlist();
+        playlist.setTitle("playlist1");
+        playlistService.createPlaylist(playlist);
+        return playlistService.getPlaylistsByTitle("playlist1").get(0);
+    }
+
+    private Album album() {
+        Album album = new Album();
+        album.setTitle("albuum");
+        album.setReleaseDate(new Date());
+        albumService.createAlbum(album);
+        return albumService.getAlbumsByTitle("albuum").get(0);
+    }
+
+}
