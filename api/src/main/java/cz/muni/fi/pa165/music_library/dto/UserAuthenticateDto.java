ffea995c44b5dcc3876dfbc07232bcda1d52d0e3
Peter Ziska <peter@ziska.sk>
2019-01-11 21:01:03 +0100

    Add equals and hashcode for UserAuthenticateDto

--- api/src/main/java/cz/muni/fi/pa165/music_library/dto/UserAuthenticateDto.java
+++ api/src/main/java/cz/muni/fi/pa165/music_library/dto/UserAuthenticateDto.java
@@ -25,4 +25,22 @@ public class UserAuthenticateDto {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAuthenticateDto)) return false;
+
+        UserAuthenticateDto userAuthenticateDto = (UserAuthenticateDto) o;
+
+        if (!getEmail().equals(userAuthenticateDto.getEmail())) return false;
+        return getPassword() == userAuthenticateDto.getPassword();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getPassword().hashCode();
+        result = 31 * result + (getEmail() != null ? getEmail().hashCode() : 0);
+        return result;
+    }
 }
